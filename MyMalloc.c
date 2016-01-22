1fdc5978e5faa758487ff8a12961492ec6d40f20
yshiroya <yshiroya@purdue.edu>
2016-01-22 10:07:05 -0500

    80 of 100

--- MyMalloc.c
+++ MyMalloc.c
@@ -350,7 +350,7 @@ void freeObject( void * ptr )
 		} 
 	}
 	else if(coal_both == 2) {
-		left_header->_allocated = 0;
+		/*left_header->_allocated = 0;
 		left_header->_objectSize = left_header->_objectSize + current_header->_objectSize + right_header->_objectSize;
 		
 		right_footer->_allocated = 0;
@@ -358,7 +358,7 @@ void freeObject( void * ptr )
 		
 		left_header->_next = left_header->_next->_next;
 		right_header->_next->_prev = left_header;
-		pointer = left_header;
+		pointer = left_header;*/
 	}
 	
 	struct ObjectHeader * p = _freeList->_next;
